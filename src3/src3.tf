886a59755476a312405350c928441d8defe948a3
Zain Hasan <44829199+zainq11@users.noreply.github.com>
2025-04-02 11:30:31 -0700

    should not show

--- src3/src3.tf
+++ src3/src3.tf
@@ -3,3 +3,4 @@ resource "null_resource" "src3_r2" {}
 resource "null_resource" "src3_r3" {}
 resource "null_resource" "src3_r4" {}
 resource "null_resource" "src3_r5" {}
+resource "null_resource" "src3_r6" {}
