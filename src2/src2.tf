54f401b5a6460a9dd177f96f1621fe20d5687c96
Zain Hasan <44829199+zainq11@users.noreply.github.com>
2025-06-27 11:51:26 -0700

    src2_r9

--- src2/src2.tf
+++ src2/src2.tf
@@ -6,3 +6,4 @@ resource "null_resource" "src2_r5" {}
 resource "null_resource" "src2_r6" {}
 resource "null_resource" "src2_r7" {}
 resource "null_resource" "src2_r8_1xxxx" {}
+resource "null_resource" "src2_r9" {}
