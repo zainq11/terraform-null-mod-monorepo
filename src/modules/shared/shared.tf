00b30ebdeef84f4bcbc762be840643711c0d7097
Zain Hasan <44829199+zainq11@users.noreply.github.com>
2025-08-28 16:59:52 -0700

    using remote global module

--- src/modules/shared/shared.tf
+++ src/modules/shared/shared.tf
@@ -1,5 +1,10 @@
-module "global_1" {
-    source = "../../../global"
+//module "global_1" {
+//    source = "../../../global"
+//}
+
+module "global" {
+  source  = "app.terraform.io/zaincorp/global/null"
+  version = "1.0.0"
 }
 
 resource "null_resource" "shared_r1" {}
